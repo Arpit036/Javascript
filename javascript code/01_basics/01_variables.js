d71c372baf1fe0141ac29e3e740610ea5b3b453b
Arpit <arpitvermaji22@gmail.com>
2024-03-06 13:17:19 +0530

    01_variable

--- javascript code/01_basics/01_variables.js
+++ javascript code/01_basics/01_variables.js
@@ -1,7 +1,7 @@
 const accountId = 144553
-let accountEmail = "hitesh@google.com"
+let accountEmail = "arpi@gmail.com"
 var accountPassword = "12345"
-accountCity = "Jaipur"
+accountCity = "Lakhimpur"
 let accountState;
 
 // accountId = 2 // not allowed
